f3b55c299089d5b035daa570259f85f891f9f115
Dominik Mazak <d.mazak11@gmail.com>
2020-03-30 16:28:33 +0200

    pulled out the api key into ini file

--- apimanager.py
+++ apimanager.py
@@ -1,19 +1,22 @@
 import requests
 import json
+import configparser
 
-class APIManager(object):
+class self(object):
     
-    api_key = 'RGAPI-85a3a340-e04a-4596-a066-574996513cf2'
     def __init__(self, summoner_name):
         self.summoner_name = 'Worst Lux Galaxy'
         # create base url dictionary for API calls
         with open ('jsons\\base_urls.json', encoding="utf8") as JSON:
             self.base_urls = json.load(JSON)
+        config = configparser.ConfigParser()
+        config.read('config.ini')
+        self.api_key = config['API']['key']    
     
     def get_summoner_info(self):
         """Tries to retrieve encrypted account ID based on summoner name"""
         url = self.base_urls['summoner'] + self.summoner_name
-        headers = {'X-Riot-Token': APIManager.api_key}
+        headers = {'X-Riot-Token': self.api_key}
 
         r = requests.get(url, headers = headers)
         if not (r.status_code == 200):
@@ -24,7 +27,7 @@ class APIManager(object):
 
     def get_match_history(self, accountId):
         url = self.base_urls['matchhistory'] + accountId + '?queue=420'
-        headers = {'X-Riot-Token': APIManager.api_key}
+        headers = {'X-Riot-Token': self.api_key}
 
         r = requests.get(url, headers = headers)
         if not (r.status_code == 200):
@@ -35,7 +38,7 @@ class APIManager(object):
     
     def get_match_details(self, matchId):
         url = self.base_urls['matchdetail'] + str(matchId)
-        headers = {'X-Riot-Token': APIManager.api_key}
+        headers = {'X-Riot-Token': self.api_key}
 
         r = requests.get(url, headers = headers)
         if not (r.status_code == 200):
